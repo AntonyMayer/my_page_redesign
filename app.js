90d3f42db2b57acf4ba1be11cee514ad048814b9
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-05-06 00:44:20 -0500

    synchronizing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ class Fun {
         self = this;
         self.canvas = document.getElementsByClassName('b_fun')[0];
         self.elms = [];
+        self.random = 0;
     }
     init(number) {
         self.build(number);
@@ -14,8 +15,8 @@ class Fun {
         for (let i = 0; i < number; i++) {
             let elm = document.createElement('div');
             elm.className = 'b_fun__elm';
-            elm.style.width = `${50/number}%`;
-            elm.style.margin = `0 ${25/number}%`;
+            elm.style.width = `${30/number}%`;
+            elm.style.margin = `0 ${15/number}%`;
             self.elms.push(elm);
             self.canvas.appendChild(elm);
         }
@@ -24,17 +25,21 @@ class Fun {
        setInterval(self.iterate, 400);
     }
     iterate() {
-        for (var i = 0, maxI = self.elms.length; i < maxI; i++) {
+        for (let i = 0, maxI = self.elms.length; i < maxI; i+= 30) {
             self.setRandomHeight(self.elms[i]);
+            for (let z = 0; z < 30; z++) {
+                self.elms[i+z].style.height = `${self.random*0.9}%`;
+                self.elms[i+z].style.top = `${-self.random*0.45}%`;
+            }
         }
     }
     setRandomHeight(elm) {
-        let random = Math.random() * 100;
-        elm.style.height = `${random}%`;
-        elm.style.top = `${-random/2}%`;
+        self.random = Math.random() * 100;
+        elm.style.height = `${self.random}%`;
+        elm.style.top = `${-self.random/2}%`;
     }
 }
 
 var fun = new Fun();
 
-fun.init(window.innerWidth/5);
+fun.init(window.innerWidth/10);
